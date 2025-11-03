ecbd10c15da8847fb008f66913ca9826088cc170
Prasad.R.S <138349149+Prasad6008@users.noreply.github.com>
2025-11-03 18:34:06 +0530

    server.js

--- backend/server.js
+++ backend/server.js
@@ -46,7 +46,7 @@ app.use("/api/notifications",notificationRoute)
 if(process.env.NODE_ENV === "production")
 {
     app.use(express.static(path.join(__dirname,'/frontend/build')))
-    app.use("*",(req,res) =>
+    app.get("*",(req,res) =>
     {
         res.sendFile(path.resolve(__dirname,"frontend","build","index.html"))
     })
